8ae7f9ef37d58357a4109164675353fa55c57f88
Jay <sijk0331@gmail.com>
2019-09-01 23:46:14 +0900

    sequelize config

---
+++ config/sequelizeConfig.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+module.exports = {
+    development : {
+        username : 'root',
+        password : process.env.MYSQL_DEVELOPMENT_PASSWORD,
+        database : 'dsm_market',
+        host : '127.0.0.1',
+        dialect : 'mysql',
+        logging : false,
+    },
+}
