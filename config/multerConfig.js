13d456df59d60508b728ac6766f00c954b078790
Jay <sijk0331@gmail.com>
2019-09-04 19:16:21 +0900

    multer config

---
+++ config/multerConfig.js
@@ -0,0 +1,18 @@
+const AWS = require('aws-sdk');
+const path = require('path');
+AWS.config.loadFromPath(path.join(__dirname, '/awsConfig.json'));
+const S3 = new AWS.S3();
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+
+module.exports = multer({
+    storage : multerS3({
+        s3 : S3,
+        bucket : "dsmmarket",
+        key : (req, file, cb) => {
+            const name = new Date().valueOf() + path.extname(file.originalname);
+            cb(null, name);
+        },
+        acl : 'public-read-write',
+    }),
+});
