6c3417645105d179f36cc379dc955a2a3da92768
Jay <sijk0331@gmail.com>
2019-09-05 14:39:04 +0900

    delete configFiles

--- config/awsConfig.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    aws_access_key_id : process.env.AWS_ACCESS_KEY,
-    aws_secret_access_key : process.env.AWS_SECRET_KEY,
-    region : 'ap-northeast-2'
-};
