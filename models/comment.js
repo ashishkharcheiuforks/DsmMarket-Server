532f6ba3c903fbbf0e66f0d95358dab8662fa838
Jay <sijk0331@gmail.com>
2019-09-12 20:13:18 +0900

    add email field

--- models/comment.js
+++ models/comment.js
@@ -1,5 +1,9 @@
 module.exports = (sequelize, DataTypes) => {
     return sequelize.define('comment', {
+        email : {
+            type : DataTypes.STRING(40),
+            allowNull : false,
+        },
         nick : {
             type : DataTypes.STRING(40),
             allowNull : false,
