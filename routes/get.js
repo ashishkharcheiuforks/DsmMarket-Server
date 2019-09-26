64de2e44a4ce3229817def72809feee098fe7eff
Jay <sijk0331@gmail.com>
2019-09-26 19:23:29 +0900

    modify

--- routes/get.js
+++ routes/get.js
@@ -495,7 +495,6 @@ router.get('/list/recommend', verifyToken, async (req, res, next) => {
 });
 router.get('/user/list/deal', verifyToken, async (req, res, next) => {
     const userId = req.app.get('user').userId;
-    const postId = req.body.postId;
     try {
         const posts = await DealPost.findAll({
             where : {userId},
@@ -522,7 +521,6 @@ router.get('/user/list/deal', verifyToken, async (req, res, next) => {
 });
 router.get('/user/list/rent', verifyToken, async (req, res, next) => {
     const userId = req.app.get('user').userId;
-    const postId = req.body.postId;
     try {
         const posts = await RentPost.findAll({
             where : {userId},
