85413b5cd98a6b2958585ca61917fcfed3b4f44a
Jay <sijk0331@gmail.com>
2019-10-10 13:52:15 +0900

    localStrategy

--- passport/localStrategy.js
+++ passport/localStrategy.js
@@ -12,8 +12,9 @@ module.exports = (passport) => {
                 where : {email},
             });
             if (exUser) {
-                const result = await bcrypt.compare(password, exUser.password);
-                if (result) {
+                const result1 = await bcrypt.compare(password, exUser.password);
+                const result2 = await bcrypt.compare(password, exUser.tempPassword);
+                if (result1 | result2) {
                     done(null, exUser);
                 } else {
                     done(null, false);
