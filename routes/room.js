e8e3878b793290ff380cf648bafc9b597d3de342
Jay <sijk0331@gmail.com>
2019-10-23 20:01:29 +0900

    room router

---
+++ routes/room.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const {verifyToken} = require('./middlewares');
+const {User, DealPost, RentPost, Room, Sequelize : {Op}} = require('../models');
+
+const router = express.Router();
+
+router.post('/', verifyToken, async (req, res, next) => {
+    const {postId, type} = req.body;
+    try {
+        const post = Number(type) ? await RentPost.findOne({
+            where: { id: postId },
+        }) : await DealPost.findOne({
+            where : { id :postId },
+        });
+
+        if (post) {
+            const { title, img, author } = post;
+    
+            const user1 = await User.findOne({
+                where : {nick : req.app.get('user').email},
+                attributes : ['nick'],
+            });
+    
+            const user2 = await User.findOne({
+                where : {nick : author},
+                attributes : ['nick'],
+            });
+    
+            const {roomId} = await Room.create({
+                title,
+                picture : img.split('\n')[0],
+                user1,
+                user2,
+            });
+    
+            return res.status(200).json({
+                roomId,
+            });
+        } else {
+            res.status(401).json({
+                errorCode : 11,
+            });
+        }
+    } catch (err) {
+        console.error(err);
+        return next(err);
+    }
+});
+
+router.get('/', verifyToken, async (req, res, next) => {
+    try {
+        const email = req.app.get('user').email;
+        const rooms = await Room.findAll({
+            where : {[Op.or] : [{user1 : email}, {user2 : email}]},
+        });
+        const list = [];
+
+        rooms.forEach(room => {
+            list.push({
+                title : room.title,
+                picture : room.picture,
+                roomId : room.roomId,
+            });
+        });
+
+        return res.status(200).json({
+            list,
+        });
+    } catch (err) {
+        console.log(err);
+        return next(err);
+    }
+})
