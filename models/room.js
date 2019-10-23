4c9e00bc8a5b74c0cc00ed6407b972a48ba3a3b4
Jay <sijk0331@gmail.com>
2019-10-23 20:01:06 +0900

    room model

---
+++ models/room.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+    sequelize.define('room', {
+        title : {
+            type : DataTypes.String(100),
+            allowNull : false,
+        },
+        picture : {
+            type : DataTypes.TEXT,
+            allowNull : false,
+        },
+        roomId : {
+            type : DataTypes.TEXT,
+            allowNull : false,
+            autoIncrement : true,
+        },
+        user1 : {
+            type : DataTypes.STRING(40),
+            allowNull : false,
+        },
+        user2 : {
+            type : DataTypes.STRING(40),
+            allowNull : false,
+        },
+    }, {
+        timestamps : true,
+    });
+};
