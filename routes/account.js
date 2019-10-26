e2ff59f6342eea8f96d673b55671f1984ac91561
Jay <sijk0331@gmail.com>
2019-10-26 22:35:45 +0900

    account router

--- routes/account.js
+++ routes/account.js
@@ -58,9 +58,6 @@ router.patch('/password', verifyToken, async (req, res, next) => {
         }, {
             where: { email },
         });
-        await Auth.destroy({
-            where: { email },
-        });
         return res.status(200).json({
             message: 2,
         });
