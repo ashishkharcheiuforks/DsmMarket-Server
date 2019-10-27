b08e933159630a88d7de8c23d264b814ef29bb74
Jay <sijk0331@gmail.com>
2019-10-27 12:22:09 +0900

    get router

--- routes/get.js
+++ routes/get.js
@@ -491,8 +491,8 @@ router.get('/list/recommend', verifyToken, async (req, res, next) => {
             }
         });
         const list = [];
-        console.log(posts.data.list);
         for (const post of posts.data.list) {
+            console.log(post);
             const {id, title, img} = type ? await RentPost.findOne({
                 where : {id : post.id}
             }) : await DealPost.findOne({
