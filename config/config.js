5054137d33e187fb07b217aaa6ddf3d0a8a9b142
Jay <sijk0331@gmail.com>
2019-11-06 19:42:56 +0900

    Sequelize config

--- config/config.js
+++ config/config.js
@@ -7,6 +7,10 @@ module.exports = {
         database : 'dsm_market',
         host : '127.0.0.1',
         dialect : 'mysql',
+        dialectOptions: {
+            charset: 'utf8mb4',
+            collate: 'utf8mb4_general_ci'
+          },
         logging : false,
     },
 };
