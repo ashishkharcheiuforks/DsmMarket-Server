c53d86037d3a0a64b5546a841e086cab1fc7af62
Jay <sijk0331@gmail.com>
2019-11-13 16:57:47 +0900

    report router

--- routes/report.js
+++ routes/report.js
@@ -5,7 +5,6 @@ const moment = require('moment');
 require('dotenv').config();
 
 const router = express.Router();
-
 const transporter = nodemailer.createTransport({
     service : 'gmail',
     auth : {
@@ -14,47 +13,55 @@ const transporter = nodemailer.createTransport({
     },
 });
 
-router.post('/post', verifyToken, (req, res, next) => {
-    const {postId, type, reason} = req.body;
-    const mailOptions = {
-        from : 'dsmplanb@gmail.com',
-        to : 'dsmplanb@gmail.com',
-        subject : moment().format("YYYY년 MM월 DD일 HH시 mm분 게시물 신고"),
-        text : `신고자 ID :${req.app.get('user').email}
-        게시물 타입 : ${Number(type) ? '대여글' : '거래글'}
-        게시물 ID : ${postId}
-        신고사유 : ${reason}`,
-    };
-    transporter.sendMail(mailOptions, (err, info) => {
-        if (err) {
-            console.error(err);
-            return next(err);
-        }
-    });
-    return res.status(200).json({
-        message : 13,
-    });
+router.post('/post', verifyToken, async (req, res, next) => {
+    try {
+        const { postId, type, reason } = req.body;
+        const mailOptions = {
+            from: 'dsmplanb@gmail.com',
+            to: 'dsmplanb@gmail.com',
+            subject: moment().format("YYYY년 MM월 DD일 HH시 mm분 게시물 신고"),
+            text: `신고자 ID :${req.app.get('user').email}
+            게시물 타입 : ${Number(type) ? '대여글' : '거래글'}
+            게시물 ID : ${postId}
+            신고사유 : ${reason}`,
+        };
+        
+        await transporter.sendMail(mailOptions);
+        
+        return res.status(200).json({
+            success: true,
+            message : 'report success',
+        });
+    } catch (err) {
+        console.error(err);
+        return next(err);
+    }
 });
+
 router.post('/comment', verifyToken, (req, res, next) => {
-    const {postId, type, nick, reason} = req.body;
-    const mailOptions = {
-        from : 'dsmplanb@gmail.com',
-        to : 'dsmplanb@gmail.com',
-        subject : moment().format("YYYY년 MM월 DD일 HH시 mm분 댓글 신고"),
-        text : `신고자 ID :${req.app.get('user').email}
-        게시물 타입 : ${Number(type) ? '대여글' : '거래글'}
-        게시물 ID : ${postId}
-        댓글 작성자 : ${nick}
-        사유 : ${reason}`,
-    };
-    transporter.sendMail(mailOptions, (err, info) => {
-        if (err) {
-            console.error(err);
-            return next(err);
-        }
-    });
-    return res.status(200).json({
-        message : 13,
-    });
+    try {
+        const { postId, type, nick, reason } = req.body;
+        const mailOptions = {
+            from: 'dsmplanb@gmail.com',
+            to: 'dsmplanb@gmail.com',
+            subject: moment().format("YYYY년 MM월 DD일 HH시 mm분 댓글 신고"),
+            text: `신고자 ID :${req.app.get('user').email}
+            게시물 타입 : ${Number(type) ? '대여글' : '거래글'}
+            게시물 ID : ${postId}
+            댓글 작성자 : ${nick}
+            사유 : ${reason}`,
+        };
+        
+        await transporter.sendMail(mailOptions);
+        
+        return res.status(200).json({
+            success: true,
+            message : 'report success',
+        });
+    } catch (err) {
+        console.error(err);
+        return next(err);
+    }
 });
+
 module.exports = router;
