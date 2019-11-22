74ca918f2b0090b4027f8ca2b3aa9799e3f9cbda
Jay <sijk0331@gmail.com>
2019-11-22 19:36:10 +0900

    index.js

--- models/index.js
+++ models/index.js
@@ -3,7 +3,12 @@ const config = require('../config/config').development;
 const db = {};
 
 const sequelize = new Sequelize(
-    config.database, config.username, config.password, config,
+    config.database, config.username, config.password, {
+        timezone : config.timezone,
+        host : config.host,
+        dialect : config.dialect,
+        logging : config.logging,
+    }
 );
 
 db.sequelize = sequelize;
