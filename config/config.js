967b1ab0521e65a8ec676f37b30e096d0806d20b
Jay <sijk0331@gmail.com>
2019-11-22 19:35:59 +0900

    sequelize config

--- config/config.js
+++ config/config.js
@@ -8,8 +8,6 @@ module.exports = {
         host : '127.0.0.1',
         dialect : 'mysql',
         logging : false,
-        options : {
-            timezone : '+09:00',
-        },
+        timezone : '+09:00',
     },
 };
