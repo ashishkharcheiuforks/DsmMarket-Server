24863451851cac1a1cdc4dcb1eb1bc9feb213332
Jay <sijk0331@gmail.com>
2019-11-25 16:12:07 +0900

    interest model

--- models/interest.js
+++ models/interest.js
@@ -1,20 +1,20 @@
 module.exports = (sequelize, DataTypes) => {
     return sequelize.define('interest', {
-        userId : {
-            type : DataTypes.INTEGER,
-            allowNull : false,
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
         },
-        postId : {
-            type : DataTypes.INTEGER,
-            allowNull : false,
+        postId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
         },
-        type : {
-            type : DataTypes.INTEGER,
-            allowNull : false,
+        type: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
         },
     }, {
-        charset : 'utf8',
-        collation : 'utf8_general_ci',
-        timestamps : true,
+        charset: 'utf8',
+        collation: 'utf8_general_ci',
+        timestamps: true,
     });
 };
