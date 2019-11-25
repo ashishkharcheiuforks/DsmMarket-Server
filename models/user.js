447882310bba637b9942c75636de8ffc77c8d3eb
Jay <sijk0331@gmail.com>
2019-11-25 16:12:57 +0900

    user model

--- models/user.js
+++ models/user.js
@@ -1,42 +1,42 @@
 module.exports = (sequelize, DataTypes) => {
     return sequelize.define('user', {
-        email : {
-            type : DataTypes.STRING(40),
-            allowNull : false,
-            unique : true,
+        email: {
+            type: DataTypes.STRING(40),
+            allowNull: false,
+            unique: true,
         },
-        password : {
-            type : DataTypes.STRING(100),
-            allowNull : false,
+        password: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
         },
-        tempPassword : {
-            type : DataTypes.STRING(100),
-            allowNull : true,
+        tempPassword: {
+            type: DataTypes.STRING(100),
+            allowNull: true,
         },
-        nick : {
-            type : DataTypes.STRING(100),
-            allowNull : false,
-            unique : true,
+        nick: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+            unique: true,
         },
-        grade : {
-            type : DataTypes.STRING(100),
-            allowNull : false,
+        grade: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
         },
-        gender : {
-            type : DataTypes.STRING(100),
-            allowNull : false,
+        gender: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
         },
-        dealLogs : {
-            type : DataTypes.TEXT,
-            allowNull : false,
+        dealLogs: {
+            type: DataTypes.TEXT,
+            allowNull: false,
         },
-        rentLogs : {
-            type : DataTypes.TEXT,
-            allowNull : false,
+        rentLogs: {
+            type: DataTypes.TEXT,
+            allowNull: false,
         },
     }, {
-        charset : 'utf8',
-        collation : 'utf8_general_ci',
-        timestamps : true,
+        charset: 'utf8',
+        collation: 'utf8_general_ci',
+        timestamps: true,
     });
 };
