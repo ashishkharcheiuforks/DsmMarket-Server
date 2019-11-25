b4af82c36b0b219d16df9fb3263bba080c7ff676
Jay <sijk0331@gmail.com>
2019-11-25 16:12:40 +0900

    rentPost model

--- models/rentPost.js
+++ models/rentPost.js
@@ -1,36 +1,36 @@
 module.exports = (sequelize, DataTypes) => {
     return sequelize.define('rentPost', {
-        author : {
-            type : DataTypes.STRING(40),
-            allowNull : false,
+        author: {
+            type: DataTypes.STRING(40),
+            allowNull: false,
         },
-        img : {
-            type : DataTypes.TEXT,
-            allowNull : false,
+        img: {
+            type: DataTypes.TEXT,
+            allowNull: false,
         },
-        title : {
-            type : DataTypes.STRING(100),
-            allowNull : false,
+        title: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
         },
-        content : {
-            type : DataTypes.TEXT,
-            allowNull : false,
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false,
         },
-        price : {
-            type : DataTypes.TEXT,
-            allowNull : false,
+        price: {
+            type: DataTypes.TEXT,
+            allowNull: false,
         },
-        category : {
-            type : DataTypes.STRING(100),
-            allowNull : false,
+        category: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
         },
-        possible_time : {
-            type : DataTypes.STRING(30),
-            allowNull : true,
+        possible_time: {
+            type: DataTypes.STRING(30),
+            allowNull: true,
         }
     }, {
-        charset : 'utf8',
-        collation : 'utf8_general_ci',
-        timestamps : true,
+        charset: 'utf8',
+        collation: 'utf8_general_ci',
+        timestamps: true,
     });
 };
