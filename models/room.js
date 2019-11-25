a3979ce34f16cabc7404fa1b4a092a5e480d79c9
Jay <sijk0331@gmail.com>
2019-11-25 16:12:48 +0900

    room model

--- models/room.js
+++ models/room.js
@@ -1,32 +1,32 @@
 module.exports = (sequelize, DataTypes) => {
     return sequelize.define('room', {
-        postId : {
-            type : DataTypes.INTEGER,
-            allowNull : false,
+        postId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
         },
-        type : {
-            type : DataTypes.INTEGER,
-            allowNull : false,
+        type: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
         },
-        picture : {
-            type : DataTypes.TEXT,
-            allowNull : false,
+        picture: {
+            type: DataTypes.TEXT,
+            allowNull: false,
         },
-        roomId : {
-            type : DataTypes.INTEGER,
-            allowNull : false,
-            primaryKey : true,
-            autoIncrement : true,
+        roomId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
         },
-        user1 : {
-            type : DataTypes.STRING(40),
-            allowNull : false,
+        user1: {
+            type: DataTypes.STRING(40),
+            allowNull: false,
         },
-        user2 : {
-            type : DataTypes.STRING(40),
-            allowNull : false,
+        user2: {
+            type: DataTypes.STRING(40),
+            allowNull: false,
         },
     }, {
-        timestamps : true,
+        timestamps: true,
     });
 };
