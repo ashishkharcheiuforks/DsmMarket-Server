6a22bb090579c616266354224c7c2613ea0a089d
Jay <sijk0331@gmail.com>
2019-11-25 16:11:39 +0900

    comment model

--- models/comment.js
+++ models/comment.js
@@ -1,20 +1,20 @@
 module.exports = (sequelize, DataTypes) => {
     return sequelize.define('comment', {
-        email : {
-            type : DataTypes.STRING(40),
-            allowNull : false,
+        email: {
+            type: DataTypes.STRING(40),
+            allowNull: false,
         },
-        nick : {
-            type : DataTypes.STRING(40),
-            allowNull : false,
+        nick: {
+            type: DataTypes.STRING(40),
+            allowNull: false,
         },
-        content : {
-            type : DataTypes.TEXT,
-            allowNull : false,
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false,
         },
     }, {
-        charset : 'utf8',
-        collation : 'utf8_general_ci',
-        timestamps : true,
+        charset: 'utf8',
+        collation: 'utf8_general_ci',
+        timestamps: true,
     });
 };
