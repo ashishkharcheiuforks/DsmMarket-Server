a8a78972e90a8616c4daad484ff95e456463aa9c
Jay <sijk0331@gmail.com>
2019-11-25 16:11:50 +0900

    dealPost model

--- models/dealPost.js
+++ models/dealPost.js
@@ -1,32 +1,32 @@
 module.exports = (sequelize, DataTypes) => {
     return sequelize.define('dealPost', {
-        author : {
-            type : DataTypes.STRING(40),
-            allowNull : false,
+        author: {
+            type: DataTypes.STRING(40),
+            allowNull: false,
         },
-        img : {
-            type : DataTypes.TEXT,
-            allowNull : false,
+        img: {
+            type: DataTypes.TEXT,
+            allowNull: false,
         },
-        title : {
-            type : DataTypes.STRING(100),
-            allowNull : false,
+        title: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
         },
-        content : {
-            type : DataTypes.TEXT,
-            allowNull : false,
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false,
         },
-        price : {
-            type : DataTypes.TEXT,
-            allowNull : false,
+        price: {
+            type: DataTypes.TEXT,
+            allowNull: false,
         },
-        category : {
-            type : DataTypes.STRING(100),
-            allowNull : false,
+        category: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
         },
     }, {
-        charset : 'utf8',
-        collation : 'utf8_general_ci',
-        timestamps : true,
+        charset: 'utf8',
+        collation: 'utf8_general_ci',
+        timestamps: true,
     });
 };
